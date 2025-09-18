4bd00b42c7d433a4ef3e4812f36cc0dedb6e581b
harshita-g <82271246+harshita-g@users.noreply.github.com>
2025-09-18 04:20:55 +0000

    Deploying to gh-pages from @ harshita-g/harshitagupta.github.io@d569e8f9cded4ba7af023cca8bfaa3d156fbf838 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,20 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/harshitagupta.github.io/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/harshitagupta.github.io/people/";
-          },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/harshitagupta.github.io/_pages/dropdown.html";
-          },
         },{
         id: 'social-email',
         title: 'email',
