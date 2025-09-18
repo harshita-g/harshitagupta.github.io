dfdfbfde0a8fde5789148f5ce57dadc41a0c6939
harshita-g <82271246+harshita-g@users.noreply.github.com>
2025-09-18 00:19:12 +0000

    Deploying to gh-pages from @ harshita-g/harshitagupta.github.io@1a7113538113e7c70e70881920879fab5028bc1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,23 +7,37 @@ ninja.data = [{
     title: "",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/harshitagupta.github.io/";
+      window.location.href = "/";
     },
   },{id: "nav-cv",
           title: "cv",
           description: "To download the CV, click on the PDF button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/harshitagupta.github.io/cv/";
+            window.location.href = "/cv/";
           },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/harshitagupta.github.io/people/";
+            window.location.href = "/people/";
           },
-        },{
+        },{id: "dropdown-bookshelf",
+              title: "bookshelf",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/books/";
+              },
+            },{id: "dropdown-blog",
+              title: "blog",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/blog/";
+              },
+            },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
